b4c56323f63727c396b86e0ab03343271cdfc553
BurningBaoTree <133838321+BurningBaoTree@users.noreply.github.com>
2023-09-08 17:26:05 +0900

    2023-09-08

---
+++ Assets/Scripts/ExtraScripts/ShaderGraphTextureExprot.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ShaderGraphTextureExprot : EditorWindow
+{
+    public Material shader;
+    public RenderTexture renderTexture;
+    List<string> Logs = new List<string>();
+    public GameObject obj;
+
+    string BasePath = "Asset/Textur_Out";
+
+    [MenuItem("Custom Tools / 쉐이더 그래프 텍스쳐 추출기")]
+    public static void ShowWindow()
+    {
+        GetWindow<ShaderGraphTextureExprot>("텍스쳐 추출기");
+    }
+    private void OnGUI()
+    {
+        EditorGUILayout.BeginHorizontal();
+        if (GUILayout.Button("작업 취소"))
+        {
+            Initialized();
+        }
+        EditorGUILayout.EndHorizontal();
+        shader = EditorGUILayout.ObjectField(obj,typeof(Material),true)as Material;
+
+
+    }
+
+    private void Initialized()
+    {
+
+    }
+
+
+
+
+
+}
