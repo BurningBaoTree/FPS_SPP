6237400a424f14a504ee46b8c93968aa86fbf262
RaccoonCode <133838321+BurningBaoTree@users.noreply.github.com>
2023-09-12 15:12:04 +0900

    2023-09-12

---
+++ Assets/Scripts/Player/Equiped.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Equiped : MonoBehaviour
+{
+    public GameObject LeftWeaponSlot;
+    public GameObject RightWeaponSlot;
+}
