b7a112ac6db1b2557621cbfefda3a7c7b8fe28be
RaccoonCode <133838321+BurningBaoTree@users.noreply.github.com>
2023-09-15 15:21:41 +0900

    2023-09-15

---
+++ Assets/Scripts/Core/CoolTimeSys.cs
@@ -0,0 +1,149 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoolTimeSys : Singleton<CoolTimeSys>
+{
+    Action UpdateCooltimer;
+
+    public float timecounter = 0;
+
+    public float cooltimer1 = 0;
+    public float cooltimer2 = 0;
+    public float cooltimer3 = 0;
+    public float cooltimer4 = 0;
+    public float cooltimer5 = 0;
+
+    public bool coolActive1 = false;
+    public bool coolActive2 = false;
+    public bool coolActive3 = false;
+    public bool coolActive4 = false;
+    public bool coolActive5 = false;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        UpdateCooltimer = WeWantNoNull;
+    }
+
+    private void Update()
+    {
+        UpdateCooltimer();
+    }
+
+    void timecouting()
+    {
+        timecounter += Time.deltaTime;
+    }
+    void coolTimerSys1()
+    {
+        if (cooltimer1 <= timecounter)
+        {
+            coolActive1 = false;
+            cooltimer1 = 0;
+            UpdateCooltimer -= coolTimerSys1;
+        }
+    }
+    void coolTimerSys2()
+    {
+        if (cooltimer2 <= timecounter)
+        {
+            coolActive2 = false;
+            cooltimer2 = 0;
+            UpdateCooltimer -= coolTimerSys2;
+        }
+    }
+    void coolTimerSys3()
+    {
+        if (cooltimer3 <= timecounter)
+        {
+            coolActive3 = false;
+            cooltimer3 = 0;
+            UpdateCooltimer -= coolTimerSys3;
+        }
+    }
+    void coolTimerSys4()
+    {
+        if (cooltimer4 <= timecounter)
+        {
+            coolActive4 = false;
+            cooltimer4 = 0;
+            UpdateCooltimer -= coolTimerSys3;
+        }
+    }
+    void coolTimerSys5()
+    {
+        if (cooltimer3 <= timecounter)
+        {
+            coolActive3 = false;
+            cooltimer5 = 0;
+            UpdateCooltimer -= coolTimerSys3;
+        }
+    }
+    public void cooltimeStart(int cas, float time)
+    {
+        switch (cas)
+        {
+            case 1:
+                coolActive1 = true;
+                UpdateCooltimer += coolTimerSys1;
+                cooltimer1 = timecounter + time;
+                break;
+            case 2:
+                coolActive2 = true;
+                UpdateCooltimer += coolTimerSys2;
+                cooltimer2 = timecounter + time;
+                break;
+            case 3:
+                coolActive3 = true;
+                UpdateCooltimer += coolTimerSys3;
+                cooltimer3 = timecounter + time;
+                break;
+            case 4:
+                coolActive4 = true;
+                UpdateCooltimer += coolTimerSys4;
+                cooltimer4 = timecounter + time;
+                break;
+            case 5:
+                coolActive4 = true;
+                UpdateCooltimer += coolTimerSys5;
+                cooltimer5 = timecounter + time;
+                break;
+            default:
+                Debug.LogWarning("쿨타임 시작 실패");
+                break;
+        }
+        if (!coolActive1 && !coolActive2 && !coolActive3 && !coolActive4 && !coolActive5)
+        {
+            UpdateCooltimer += timecouting;
+            UpdateCooltimer += AllCheckTime;
+        }
+    }
+    private void AllCheckTime()
+    {
+        if (!coolActive1 && !coolActive2 && !coolActive3 && !coolActive4 && !coolActive5)
+        {
+            UpdateCooltimer -= AllCheckTime;
+            allcoolStop();
+        }
+    }
+    /// <summary>
+    /// 모든 쿨타임을 종료합니다.
+    /// </summary>
+    public void allcoolStop()
+    {
+        UpdateCooltimer -= timecouting;
+        coolActive1 = false;
+        cooltimer1 = 0f;
+        coolActive2 = false;
+        cooltimer2 = 0f;
+        coolActive3 = false;
+        cooltimer3 = 0f;
+        coolActive4 = false;
+        cooltimer4 = 0f;
+        coolActive5 = false;
+        cooltimer5 = 0f;
+        timecounter = 0;
+    }
+}
--- Assets/Scripts/Player/Equiped.cs
+++ Assets/Scripts/Player/Equiped.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 
 public class Equiped : MonoBehaviour
 {
+    CoolTimeSys coolsys;
+
     public GameObject LeftWeaponSlot;
     public GameObject RightWeaponSlot;
 
@@ -20,15 +23,44 @@ public class Equiped : MonoBehaviour
 
     public int previous;
 
+    public Transform rightTartge;
+    public Transform leftTartge;
+
+    int layerState = 0;
+
     private void Awake()
     {
         animator = GetComponent<Animator>();
         equiptableList = new Equipments[6];
     }
+    private void OnEnable()
+    {
+        coolsys = CoolTimeSys.Inst;   
+    }
     private void Start()
     {
-        animator.SetLayerWeight(2, 0);
+        animator.SetLayerWeight(1, 0);
     }
+
+    private void OnAnimatorIK()
+    {
+        if (layerState == 1) 
+        {
+            animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
+            animator.SetIKPosition(AvatarIKGoal.RightHand, rightTartge.position);
+            animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1);
+            animator.SetIKRotation(AvatarIKGoal.RightHand, rightTartge.rotation);
+            animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);
+            animator.SetIKPosition(AvatarIKGoal.LeftHand, leftTartge.position);
+            animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1);
+            animator.SetIKRotation(AvatarIKGoal.LeftHand, leftTartge.rotation);
+        }
+        else
+        {
+
+        }
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.CompareTag("Weapon"))
@@ -58,43 +90,52 @@ public class Equiped : MonoBehaviour
 
     public void HoldThis(int num)
     {
-        if(equiptableList[num] != null && equiptableList[previous] == null)
+        if(!coolsys.coolActive1)
         {
-            equiptableList[num].gameObject.SetActive(true);
-            previous = num;
-            animator.SetLayerWeight(2, 1);
-        }
-        else if (equiptableList[num] != null && num != previous)
-        {
-            equiptableList[num].gameObject.SetActive(true);
-            equiptableList[previous].gameObject.SetActive(false);
-            previous = num;
-            animator.SetLayerWeight(2, 1);
-        }
-        else if (equiptableList[num] == null)
-        {
-            previous = 0;
-            animator.SetLayerWeight(2, 0);
-        }
-        else if (num == previous)
-        {
-            equiptableList[num].gameObject.SetActive(false);
-            previous = 5;
-            animator.SetLayerWeight(2, 0);
-        }
-        else
-        {
+            if (equiptableList[num] != null && equiptableList[previous] == null)
+            {
+                equiptableList[num].gameObject.SetActive(true);
+                previous = num;
+                animator.SetLayerWeight(1, 1);
+                layerState = 1;
+            }
+            else if (equiptableList[num] != null && num != previous)
+            {
+                equiptableList[num].gameObject.SetActive(true);
+                equiptableList[previous].gameObject.SetActive(false);
+                previous = num;
+                animator.SetLayerWeight(1, 1);
+                layerState = 1;
+            }
+            else if (equiptableList[num] == null)
+            {
+                previous = 0;
+                animator.SetLayerWeight(1, 0);
+                layerState = 0;
+            }
+            else if (num == previous)
+            {
+                equiptableList[num].gameObject.SetActive(false);
+                previous = 5;
+                animator.SetLayerWeight(1, 0);
+                layerState = 0;
+            }
+            else
+            {
 
+            }
         }
     }
     public void DropThis()
     {
         if(equiptableList[previous] != null)
         {
+            coolsys.cooltimeStart(1, 0.5f);
             equiptableList[previous].transform.parent = null;
             equiptableList[previous].DisEquiped.Invoke();
             equiptableList[previous] = null;
-            animator.SetLayerWeight(2, 0);
+            animator.SetLayerWeight(1, 0);
+            layerState = 0;
         }
     }
 
